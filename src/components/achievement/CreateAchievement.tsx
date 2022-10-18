897d7863a8c50ad648de378228a9840b2f79e57e
LJstudent <poepjan123_2@hotmail.com>
2022-10-18 04:31:51 +0200

    Add creation of achievement to project

--- src/components/achievement/CreateAchievement.tsx
+++ src/components/achievement/CreateAchievement.tsx
@@ -1,11 +1,55 @@
 import Dialog from "@mui/material/Dialog";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { IList } from "../../models/interfaces/IList";
 import { RootState } from '../../state/store';
 import './styled/CreateAchievement.scss';
-
+import * as yup from 'yup';
+import { Formik, FormikHelpers } from "formik";
+import { addItem } from "../../state/list/listSlice";
 
 function CreateAchievement() {
+    const dispatch = useDispatch();
     const open = useSelector((state: RootState) => state.list.newRecord);
+    const id = useSelector((state: RootState) => Math.max(...state.list.list.map(item => item.id)));
+
+
+    const validationSchema = yup.object({
+        title: yup
+            .string()
+            .required("Titel is verplicht"),
+        img: yup
+            .string()
+            .required("Link is verplicht")
+            .test(
+                "link-check",
+                "Link moet van unsplash zijn",
+                (img) => img !== undefined && img.includes("https://images.unsplash.com/photo-")
+            )
+    });
+
+    const formikProps = {
+        initialValues: {
+            id: 0,
+            title: "",
+            img: "",
+
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values: IList, actions: FormikHelpers<IList>) => {
+            values.id = id + 1;
+            values.title = values.title.toLowerCase();
+
+            if (values.img.includes('?')) {
+                values.img = values.img.split('?')[0];
+            }
+
+            dispatch(addItem(values));
+
+            actions.resetForm({
+                values: formikProps.initialValues
+            });
+        },
+    };
 
     return (
         <Dialog
@@ -14,23 +58,49 @@ function CreateAchievement() {
             aria-describedby="alert-dialog-slide-description"
             className="Card-Dialog"
         >
-            <form className="card-form">
-                <div className="input">
-                    <input type="text" className="input-field" />
-                    <label className="input-label">Full name</label>
-                </div>
-                <div className="input">
-                    <input type="text" className="input-field" />
-                    <label className="input-label">Email</label>
-                </div>
-                <div className="input">
-                    <input type="password" className="input-field" />
-                    <label className="input-label">Password</label>
-                </div>
-                <div className="action">
-                    <button className="action-button">Get started</button>
-                </div>
-            </form>
+            <Formik {...formikProps}>
+                {({
+                    values,
+                    handleChange,
+                    errors,
+                    handleSubmit,
+                    touched,
+                }) => {
+                    const titleValue = values.title !== "" && true;
+                    const imgValue = values.img !== "" && true;
+
+                    return (
+                        <form className="Card-Form" onSubmit={handleSubmit}>
+                            <div className="Input">
+                                <input
+                                    type="text"
+                                    className="Input-Field"
+                                    name="title"
+                                    id="title"
+                                    onChange={handleChange}
+                                    value={values.title}
+                                />
+                                <label className={titleValue ? "Input-Label-Focus" : "Input-Label"}>Titel</label>
+                            </div>
+                            {errors.title && touched.title ? (<div className="Input-Error">{errors.title}</div>) : null}
+                            <div className="Input">
+                                <input
+                                    type="text"
+                                    className="Input-Field"
+                                    name="img"
+                                    id="img"
+                                    onChange={handleChange}
+                                    value={values.img} />
+                                <label className={imgValue ? "Input-Label-Focus" : "Input-Label"}>Unsplash link</label>
+                            </div>
+                            {errors.img && touched.img ? (<div className="Input-Error">{errors.img}</div>) : null}
+                            <div className="Action">
+                                <button className="Action-Button">Zet erbij</button>
+                            </div>
+                        </form>
+                    )
+                }}
+            </Formik>
         </Dialog >
     );
 }
